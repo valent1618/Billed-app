9742d707a28dd65523cfb03a21819f64ad2dfb51
valent1618 <valentinlemaire1618@gmail.com>
2022-05-23 21:07:15 +0200

    fix bug file extension

--- src/containers/NewBill.js
+++ src/containers/NewBill.js
@@ -6,24 +6,40 @@ export default class NewBill {
     this.document = document;
     this.onNavigate = onNavigate;
     this.store = store;
+    const file = this.document.querySelector(`input[data-testid="file"]`);
+    file.addEventListener('change', this.handleChangeFile);
     const formNewBill = this.document.querySelector(
       `form[data-testid="form-new-bill"]`
     );
     formNewBill.addEventListener('submit', this.handleSubmit);
-    const file = this.document.querySelector(`input[data-testid="file"]`);
-    file.addEventListener('change', this.handleChangeFile);
     this.fileUrl = null;
     this.fileName = null;
     this.billId = null;
     new Logout({ document, localStorage, onNavigate });
   }
+
   handleChangeFile = (e) => {
     e.preventDefault();
+
+    const correctExtension = ['jpg', 'jpeg', 'png'];
+
+    const fileName = e.target.files[0].name;
+    const extension = e.target.files[0].type.split('/')[1];
+
+    if (!correctExtension.includes(extension)) {
+      e.target.setCustomValidity('Use only jpg, jpeg or png');
+    } else {
+      e.target.setCustomValidity('');
+      this.fileName = fileName;
+    }
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    const formData = new FormData();
     const file = this.document.querySelector(`input[data-testid="file"]`)
       .files[0];
-    const filePath = e.target.value.split(/\\/g);
-    const fileName = filePath[filePath.length - 1];
-    const formData = new FormData();
     const email = JSON.parse(localStorage.getItem('user')).email;
     formData.append('file', file);
     formData.append('email', email);
@@ -37,40 +53,37 @@ export default class NewBill {
         },
       })
       .then(({ fileUrl, key }) => {
-        console.log(fileUrl);
-        this.billId = key;
         this.fileUrl = fileUrl;
-        this.fileName = fileName;
+        this.billId = key;
+
+        const bill = {
+          email,
+          type: e.target.querySelector(`select[data-testid="expense-type"]`)
+            .value,
+          name: e.target.querySelector(`input[data-testid="expense-name"]`)
+            .value,
+          amount: parseInt(
+            e.target.querySelector(`input[data-testid="amount"]`).value
+          ),
+          date: e.target.querySelector(`input[data-testid="datepicker"]`).value,
+          vat: e.target.querySelector(`input[data-testid="vat"]`).value,
+          pct:
+            parseInt(
+              e.target.querySelector(`input[data-testid="pct"]`).value
+            ) || 20,
+          commentary: e.target.querySelector(
+            `textarea[data-testid="commentary"]`
+          ).value,
+          fileUrl: this.fileUrl,
+          fileName: this.fileName,
+          status: 'pending',
+        };
+        this.updateBill(bill);
+        this.onNavigate(ROUTES_PATH['Bills']);
       })
       .catch((error) => console.error(error));
-  };
-  handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(
-      'e.target.querySelector(`input[data-testid="datepicker"]`).value',
-      e.target.querySelector(`input[data-testid="datepicker"]`).value
-    );
-    const email = JSON.parse(localStorage.getItem('user')).email;
-    const bill = {
-      email,
-      type: e.target.querySelector(`select[data-testid="expense-type"]`).value,
-      name: e.target.querySelector(`input[data-testid="expense-name"]`).value,
-      amount: parseInt(
-        e.target.querySelector(`input[data-testid="amount"]`).value
-      ),
-      date: e.target.querySelector(`input[data-testid="datepicker"]`).value,
-      vat: e.target.querySelector(`input[data-testid="vat"]`).value,
-      pct:
-        parseInt(e.target.querySelector(`input[data-testid="pct"]`).value) ||
-        20,
-      commentary: e.target.querySelector(`textarea[data-testid="commentary"]`)
-        .value,
-      fileUrl: this.fileUrl,
-      fileName: this.fileName,
-      status: 'pending',
-    };
-    this.updateBill(bill);
-    this.onNavigate(ROUTES_PATH['Bills']);
+
+    // const email = JSON.parse(localStorage.getItem('user')).email;
   };
 
   // not need to cover this function by tests
