f11f95f41964583704f78a7bf788b652c3830456
valent1618 <valentinlemaire1618@gmail.com>
2022-05-23 16:04:36 +0200

    remove useless comment

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -97,7 +97,6 @@ export default class {
   handleEditTicket(e, bill, bills) {
     if (this.counter === undefined || this.id !== bill.id) this.counter = 0;
     if (this.id === undefined || this.id !== bill.id) this.id = bill.id;
-    // $(`#open-bill${bill.id}`).attr('data-show') = 'true';
     if (this.counter === 0) {
       bills.forEach((b) => {
         $(`#open-bill${b.id}`).css({ background: '#0D5AE5' });
