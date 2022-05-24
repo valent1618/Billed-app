4ca1b2b41d5d910fa1aacfd18ad16aff9d3d939a
valent1618 <valentinlemaire1618@gmail.com>
2022-05-24 18:58:52 +0200

    handle icon vertical layout and clean code

--- src/containers/Bills.js
+++ src/containers/Bills.js
@@ -7,16 +7,27 @@ export default class {
     this.document = document;
     this.onNavigate = onNavigate;
     this.store = store;
+
+    // handle icon vertical layout
+    const divIcon1 = document.getElementById('layout-icon1');
+    const divIcon2 = document.getElementById('layout-icon2');
+    if (divIcon1) divIcon1.classList.add('active-icon');
+    if (divIcon2) divIcon2.classList.remove('active-icon');
+
+    // Add event on button new bill
     const buttonNewBill = document.querySelector(
       `button[data-testid="btn-new-bill"]`
     );
     if (buttonNewBill)
       buttonNewBill.addEventListener('click', this.handleClickNewBill);
+
+    // Add event on icon eye
     const iconEye = document.querySelectorAll(`div[data-testid="icon-eye"]`);
     if (iconEye)
       iconEye.forEach((icon) => {
         icon.addEventListener('click', () => this.handleClickIconEye(icon));
       });
+
     new Logout({ document, localStorage, onNavigate });
   }
 
@@ -26,12 +37,12 @@ export default class {
 
   handleClickIconEye = (icon) => {
     const billUrl = icon.getAttribute('data-bill-url');
+    const modaleFile = document.getElementById('modaleFile');
     const imgWidth = Math.floor($('#modaleFile').width() * 0.5);
-    $('#modaleFile')
-      .find('.modal-body')
-      .html(
-        `<div style='text-align: center;' class="bill-proof-container"><img width=${imgWidth} src=${billUrl} alt="Bill" /></div>`
-      );
+    const modalBody = modaleFile.querySelector('.modal-body');
+
+    modalBody.innerHTML = `<div style='text-align: center;' class="bill-proof-container"><img width=${imgWidth} src=${billUrl} alt="Bill" /></div>`;
+
     $('#modaleFile').modal('show');
   };
 
