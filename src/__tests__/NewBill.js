32ef984d5ab9a14c88f3cc3492b6e7a172a3cade
valent1618 <valentinlemaire1618@gmail.com>
2022-05-25 18:15:21 +0200

    Add test for form and the POST method

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -3,8 +3,6 @@
  */
 
 import { fireEvent, screen, waitFor } from '@testing-library/dom';
-import NewBillUI from '../views/NewBillUI.js';
-import NewBill from '../containers/NewBill.js';
 import mockStore from '../__mocks__/store';
 import router from '../app/Router.js';
 import { ROUTES_PATH } from '../constants/routes.js';
@@ -14,7 +12,7 @@ import userEvent from '@testing-library/user-event';
 jest.mock('../app/Store.js', () => mockStore);
 
 describe('Given I am connected as an employee', () => {
-  describe('When I am on NewBill Page', () => {
+  describe('When I navigate on NewBill Page', () => {
     beforeEach(() => {
       Object.defineProperty(window, 'localStorage', {
         value: localStorageMock,
@@ -43,7 +41,7 @@ describe('Given I am connected as an employee', () => {
       const file = screen.getByTestId('file');
       userEvent.upload(file, fakeFile);
 
-      expect(file.getAttribute('valid')).toBe('false');
+      expect(file.getAttribute('data-valid')).toBe('false');
     });
 
     test('Then I add new proof bill with correct format, the file should be valid', () => {
@@ -53,7 +51,73 @@ describe('Given I am connected as an employee', () => {
       const file = screen.getByTestId('file');
       userEvent.upload(file, fakeFile);
 
-      expect(file.getAttribute('valid')).toBe('true');
+      // checkValidity() is always false with a manual adding files
+      expect(file.getAttribute('data-valid')).toBe('true');
+    });
+
+    test('Then I click to the submit button with invalid input, the form is not valid and I should not be redirected to the Bills page', async () => {
+      // Submit the form
+      const form = screen.getByTestId('form-new-bill');
+      fireEvent.submit(form);
+
+      // Check the validity of the form
+      expect(form.getAttribute('data-valid')).toBe('false');
+
+      // Check if we are still on the same page
+      expect(screen.getByText('Envoyer une note de frais')).toBeTruthy();
+    });
+
+    test('Then I click on the submit button with valid inputs, the form is valid and I should be redirected to the Bills page with a new bill', async () => {
+      // Check the number of bills into the mocked store
+      let numberOfBills = await mockStore
+        .bills()
+        .list()
+        .then((bills) => {
+          return bills.length;
+        });
+
+      // Fill the four inputs required
+      screen.getByTestId('datepicker').value = '1997-12-02';
+      screen.getByTestId('amount').value = '777';
+      screen.getByTestId('pct').value = '7';
+      const fakeFile = new File(['facture'], 'facture.png', {
+        type: 'image/png',
+      });
+      const file = screen.getByTestId('file');
+      userEvent.upload(file, fakeFile);
+
+      // Submit the form
+      const form = screen.getByTestId('form-new-bill');
+      fireEvent.submit(form);
+
+      // Check the validity of the form
+      expect(form.getAttribute('data-valid')).toBe('true');
+
+      // Check if the new bill is add on the store
+      let newBillIsInclude = await mockStore
+        .bills()
+        .list()
+        .then((bills) => {
+          return bills.includes(mockStore.bills().defaultBill);
+        });
+      expect(newBillIsInclude).toBe(true);
+
+      // Check if the number of bills is incremented by one into the store
+      let newNumberOfBills = await mockStore
+        .bills()
+        .list()
+        .then((bills) => {
+          return bills.length;
+        });
+      expect(newNumberOfBills).toBe(numberOfBills + 1);
+
+      // Wait for the Bills page load
+      await waitFor(() => screen.getByText('Mes notes de frais'));
+      expect(screen.getByText('Mes notes de frais')).toBeTruthy();
+
+      // Check if the number of bills into the Bills page is the same as in the store
+      const billsDOM = screen.getAllByTestId('icon-eye');
+      expect(billsDOM.length).toBe(newNumberOfBills);
     });
   });
 });
