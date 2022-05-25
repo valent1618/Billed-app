19ba32af00b780c5d76c79341aeba92cf5c73130
valent1618 <valentinlemaire1618@gmail.com>
2022-05-25 14:49:55 +0200

    clean code

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -5,10 +5,13 @@
 import { fireEvent, screen, waitFor } from '@testing-library/dom';
 import NewBillUI from '../views/NewBillUI.js';
 import NewBill from '../containers/NewBill.js';
+import mockStore from '../__mocks__/store';
 import router from '../app/Router.js';
 import { ROUTES_PATH } from '../constants/routes.js';
 import { localStorageMock } from '../__mocks__/localStorage.js';
 import userEvent from '@testing-library/user-event';
+
+jest.mock('../app/Store.js', () => mockStore);
 
 describe('Given I am connected as an employee', () => {
   describe('When I am on NewBill Page', () => {
@@ -30,40 +33,24 @@ describe('Given I am connected as an employee', () => {
     });
 
     afterEach(() => {
-      document.body.removeChild(document.getElementById('root'));
+      document.body.innerHTML = '';
     });
 
     test('Then I add new proof bill with incorrect format, the file should not be valid', () => {
-      const fakeFile = new File(
-        [
-          'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8z/C/HgAGgwJ/lK3Q6wAAAABJRU5ErkJggg==',
-        ],
-        'favicon.ico',
-        {
-          type: 'image/ico',
-        }
-      );
-
+      const fakeFile = new File(['facture'], 'facture.ico', {
+        type: 'image/ico',
+      });
       const file = screen.getByTestId('file');
-
       userEvent.upload(file, fakeFile);
 
       expect(file.getAttribute('valid')).toBe('false');
     });
 
     test('Then I add new proof bill with correct format, the file should be valid', () => {
-      const fakeFile = new File(
-        [
-          'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8z/C/HgAGgwJ/lK3Q6wAAAABJRU5ErkJggg==',
-        ],
-        'favicon.png',
-        {
-          type: 'image/png',
-        }
-      );
-
+      const fakeFile = new File(['facture'], 'facture.png', {
+        type: 'image/png',
+      });
       const file = screen.getByTestId('file');
-
       userEvent.upload(file, fakeFile);
 
       expect(file.getAttribute('valid')).toBe('true');
